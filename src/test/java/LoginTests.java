b9901a2c1235f594acac170bb1c762424aea4636
Sergej Schmidt <sergej-schmidt@online.de>
2024-06-28 16:38:18 +0200

    AddItemToCartTests

--- src/test/java/LoginTests.java
+++ src/test/java/LoginTests.java
@@ -9,14 +9,28 @@ public class LoginTests extends TestBase {
   public void loginPositiveTest() {
 
 
-    click(By.cssSelector("[href='/login']"));
-    type(By.name("Email"), "testor@test.info"); //  By.id(email)
-    type(By.name("Password"), "Abc1234!");
-    click(By.cssSelector(".login-button"));
+    clickOnLogin();
+    fillLoginForm();
+    clickOnLoginButton();
 
     //assert Sign Out button is present
     Assert.assertTrue(isElementPresent(By.cssSelector(".ico-logout")));
 
 
+  }
+
+  public void fillLoginForm() {
+    type(By.name("Email"), "testor@test.info"); //  By.id(email)
+    type(By.name("Password"), "Abc1234!");
+  }
+
+  public void clickOnLogin() {
+    String cssSelector = "[href='/login']";
+    click(By.cssSelector(cssSelector));
+  }
+
+  public void clickOnLoginButton() {
+    String cssSelector = ".login-button";
+    click(By.cssSelector(cssSelector));
   }
 }
